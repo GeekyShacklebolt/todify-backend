6fa2b38ab3b2b5521bf58ab3f2247bf9e122d123
Shiva Saxena <shivasaxena911@gmail.com>
2020-11-04 23:47:48 +0530

    fix(settings): remove minor unnecessary comment

--- settings.py
+++ settings.py
@@ -141,5 +141,5 @@ REST_FRAMEWORK = {
 
 AUTH_USER_MODEL = "users.User"
 
-MEDIA_ROOT = os.path.join(BASE_DIR, '.media/')  # 'data' is my media folder
+MEDIA_ROOT = os.path.join(BASE_DIR, '.media/')
 MEDIA_URL = '/.media/'
